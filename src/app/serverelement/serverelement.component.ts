b12f5864fe33f39ba3027a4da6a52f8f025de6f0
Marwan-G <ghabin2004@gmail.com>
2017-07-28 23:15:19 +0200

    creating app components

---
+++ src/app/serverelement/serverelement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-serverelement',
+  templateUrl: './serverelement.component.html',
+  styleUrls: ['./serverelement.component.css']
+})
+export class ServerelementComponent implements OnInit {
+constructor() { }
+
+  ngOnInit() {
+  }
+
+}
